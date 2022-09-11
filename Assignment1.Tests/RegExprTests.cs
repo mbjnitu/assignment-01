e951812f28c6e070f350afcc71f7cfa808d3d0e1
mbjn <mbjn@itu.dk>
2022-09-11 19:08:11 +0200

    Coded and tested SplitLine and Resolution

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,5 +1,32 @@
 namespace Assignment1.Tests;
+using FluentAssertions;
+using Xunit;
 
 public class RegExprTests
 {
+    [Fact]
+    public void SplitLine_given_lines_of_garbage_returns_words_in_garbage() {
+
+        // Arrange
+        var lines = new string[]{"hello", "my friend im", "cool092 and92 9292 cool"};
+
+        // Act
+        var n = RegExpr.SplitLine(lines);
+
+        // Assert 
+        n.Should().BeEquivalentTo(new List<string>() {"hello", "my", "friend", "im", "cool092", "and92", "9292", "cool"});
+    }
+
+    [Fact]
+    public void Resolution_given_1920x1080_andmore_returns_tuple_1920_1080_andmore() {
+
+        // Arrange
+        var line = "1920x1080, 720x800, 12020x12020 00x00";
+
+        // Act
+        var r = RegExpr.Resolution(line);
+
+        // Assert 
+        r.Should().BeEquivalentTo(new List<(int width, int height)> {(1920, 1080), (720, 800), (12020, 12020), (00, 00)});
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,28 @@
 namespace Assignment1;
+using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) {
+        string pattern = @"[\w]+";
+        Regex rg = new Regex(pattern);
+        foreach (string l in lines) {
+            foreach (Match m in rg.Matches(l)) {
+                yield return m.Value;
+            }
+        }
+    }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions) {
+        string pattern = @"(?<width>[\d]+)x(?<height>[\d]+)";
+        Regex rg = new Regex(pattern);
+        var ms = rg.Matches(resolutions);
+        foreach (Match m in ms) {
+            var thiswidth = int.Parse(m.Groups[1].Value);
+            var thisheight = int.Parse(m.Groups[2].Value);
+            yield return (thiswidth, thisheight);
+        }
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
