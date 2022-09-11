45532e03c65d0f2aca920d5d8a6889b4e7ec31f7
mbjn <mbjn@itu.dk>
2022-09-11 18:09:32 +0200

    Coded Flatten and its test

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,23 @@
 namespace Assignment1.Tests;
+using FluentAssertions;
+using Xunit;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void flatten_given_list_of_lists_w_0to9_returns_list_0to8() {
+
+        // Arrange
+        var m = new List<List<int>>() {
+            new List<int> {0, 1, 2},
+            new List<int> {3, 4, 5},
+            new List<int> {6, 7, 8}
+        };
+
+        // Act
+        var n = Iterators.Flatten(m);
+
+        // Assert 
+        n.Should().BeEquivalentTo(new List<int>() {0, 1, 2, 3, 4, 5, 6, 7, 8});
+    }
 }
